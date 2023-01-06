51896292716e6cfd10ce29615974e043d8379321
Michal G <miki.glodek@wp.pl>
2023-01-06 12:05:16 +0100

    Add conmfiguration to allow cross-origin requests. Fix url mapping for login error.

--- src/main/java/pl/uginf/rcphrwebapp/hr/user/controller/LoginMockController.java
+++ src/main/java/pl/uginf/rcphrwebapp/hr/user/controller/LoginMockController.java
@@ -2,20 +2,22 @@ package pl.uginf.rcphrwebapp.hr.user.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.uginf.rcphrwebapp.hr.user.dto.LoginRecord;
 import pl.uginf.rcphrwebapp.hr.user.dto.UserDto;
 import pl.uginf.rcphrwebapp.hr.user.service.UserService;
 
-@RestController("/login")
+@RestController
+@RequestMapping("/login")
 @AllArgsConstructor(onConstructor_ = @Autowired)
 public class LoginMockController {
 
     private final UserService userService;
 
-    @PutMapping
+    @PostMapping
     public UserDto login(@RequestBody LoginRecord loginRecord) {
         return userService.loginUser(loginRecord);
     }
